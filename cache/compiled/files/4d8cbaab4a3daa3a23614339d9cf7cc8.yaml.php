00651daaf89bf2f84e92f7b1ab193223d2074ab4
Edward Davis <hello@edwdavis.com>
2020-06-15 13:26:10 -0700

    add BM user

---
+++ cache/compiled/files/4d8cbaab4a3daa3a23614339d9cf7cc8.yaml.php
@@ -0,0 +1,31 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Users/edward/Downloads/tgaec-main/user/accounts/edavishydro.yaml',
+    'modified' => 1592252580,
+    'data' => [
+        'state' => 'enabled',
+        'email' => 'edavis@tgaec.com',
+        'fullname' => 'Edward Davis',
+        'title' => 'Hydrologist',
+        'access' => [
+            'admin' => [
+                'super' => 'true',
+                'login' => 'true'
+            ],
+            'site' => [
+                'login' => 'true'
+            ]
+        ],
+        'hashed_password' => '$2y$10$mbc3Qedl2T2W.ZL1E.nLfO9V86Tr9PxFNgOqqOk3SOPtQuoaXhp46',
+        'password' => NULL,
+        'language' => 'en',
+        'twofa_enabled' => false,
+        'twofa_secret' => 'E3E7EVFFQGBZSIFFNEQYFMGAHEEL5LMS',
+        'avatar' => [
+            
+        ],
+        'info' => NULL,
+        'groups' => NULL
+    ]
+];
