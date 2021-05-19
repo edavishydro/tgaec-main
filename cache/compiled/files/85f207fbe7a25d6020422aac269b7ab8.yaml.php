2561e5085fd3e3e57c442b9cdefc59a3b55020c8
Edward Davis <hello@edwdavis.com>
2021-05-19 14:20:13 -0700

    fix cached node_modules and cache folders

--- cache/compiled/files/85f207fbe7a25d6020422aac269b7ab8.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Users/edward/Downloads/tgaec-main/user/config/scheduler.yaml',
-    'modified' => 1592252538,
-    'data' => [
-        
-    ]
-];
